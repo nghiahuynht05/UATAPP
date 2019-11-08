54f84c31d8420ff09b22f7915e4e0a0e255a8e99
a <a>
2019-11-08 13:56:10 +0700

    first commit

---
+++ src/test/java/UATPAXPEGASUS/Stepdefs.java
@@ -0,0 +1,28 @@
+package UATPAXPEGASUS;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
