4d6c84ee77d4791d2a49015bb045c4b1ae43958e
nghia.huynht05@gmail.com <nghia.huynht05@gmail.com>
2019-11-11 15:42:35 +0700

    Fix(Config) add file config chromedriver

--- src/test/java/UATPAXPEGASUS/Stepdefs.java
+++ src/test/java/UATPAXPEGASUS/Stepdefs.java
@@ -6,23 +6,48 @@ import cucumber.api.java.en.When;
 
 import static org.junit.Assert.*;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Stepdefs {
+    private static ChromeDriverService service;
+    private WebDriver driver;
+
     @Given("today is Sunday")
     public void today_is_Sunday() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("src/test/java/UATPAXPEGASUS/input/chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            System.out.print(e);
+        }
     }
 
     @When("I ask whether it's Friday yet")
     public void i_ask_whether_it_s_Friday_yet() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver = new RemoteWebDriver(service.getUrl(),
+                DesiredCapabilities.chrome());
     }
 
-    @Then("I should be told {string}")
-    public void i_should_be_told(String string) {
+    @Then("I should be told")
+    public void i_should_be_told() {
         // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        driver.get("http://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("webdriver");
+        assertEquals("webdriver - Google Search", driver.getTitle());
     }
 
 }
